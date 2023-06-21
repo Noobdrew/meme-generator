cdca2793e2c18a67e350918686539755cd46fcda
Ivan Marinski <noobdrew23@gmail.com>
2023-06-21 16:07:16 +0300

    removed form element

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,23 @@
 export default function Form() {
   return (
-    <div className="form-conteiner">
-      <form action="">
-        <input
-          type="text"
-          name="top-text"
-          id="top-text"
-          placeholder="Top Text"
-        />
-        <input
-          type="text"
-          name="bottom-text"
-          id="bottom-text"
-          placeholder="Bottom Text"
-        />
-        <input type="button" value="Get a new meme image" />
-      </form>
-    </div>
+    <main>
+      <div className="form-conteiner">
+        <div action="" className="form">
+          <input
+            type="text"
+            name="top-text"
+            id="top-text"
+            placeholder="Top Text"
+          />
+          <input
+            type="text"
+            name="bottom-text"
+            id="bottom-text"
+            placeholder="Bottom Text"
+          />
+          <button> Get a new meme image</button>
+        </div>
+      </div>
+    </main>
   );
 }
