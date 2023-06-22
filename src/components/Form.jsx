c79937553d273be0278072c20c3a97acc755cad9
Ivan Marinski <noobdrew23@gmail.com>
2023-06-22 15:15:16 +0300

    meme state created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,29 @@
+import memeData from "../memeData";
+import React from "react";
+
 export default function Form() {
+  const [meme, setMeme] = React.useState({
+    topText: "Top Text",
+    bottomText: "Bottom Text",
+    randomImg: "https://i.imgflip.com/30b1gx.jpg",
+  });
+
+  const [allMemeImg, setAllMemeImg] = React.useState(memeData);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(
+      Math.random() * allMemeImg.data.memes.length
+    );
+
+    const randomImg = allMemeImg.data.memes[randomNumber].url;
+    console.log(randomImg);
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        randomImg: randomImg,
+      };
+    });
+  }
   return (
     <main>
       <div className="form-conteiner">
@@ -15,9 +40,10 @@ export default function Form() {
             id="bottom-text"
             placeholder="Bottom Text"
           />
-          <button> Get a new meme image</button>
+          <button onClick={getMemeImage}> Get a new meme image</button>
         </div>
       </div>
+      <img src={meme.randomImg} alt="meme" className="meme-img" />
     </main>
   );
 }
